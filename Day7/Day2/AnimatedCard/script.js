f979b962d34d7e74f72d344c684918776b6937bc
Kush Gohil <160225182+KushGohil24@users.noreply.github.com>
2025-06-16 15:01:19 +0530

    Added Day1 to Day10 projects

--- Day7/Day2/AnimatedCard/script.js
+++
@@ -1,8 +0,0 @@
-function learnMore(name) {
-    const confirmed = confirm(`Do you want to learn more about ${name}?`);
-    if (confirmed) {
-      alert(`Great! Showing more about ${name}...`);
-    } else {
-      alert(`Okay! Maybe later.`);
-    }
-  }
--- Day7/Day4/BellIconNotification/script.js
+++
@@ -1,9 +0,0 @@
-function onYes() {
-  alert("✅ You will now receive push notifications!");
-  document.getElementById("notificationModal").style.display = "none";
-}
-
-function onNo() {
-  alert("❌ You opted out of push notifications.");
-  document.getElementById("notificationModal").style.display = "none";
-}
